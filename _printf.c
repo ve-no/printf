f385760ec939fdd76e5f23a320c3f1bdad374efb
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-15 09:50:22 +0100

    remove the utils function

--- _printf.c
+++ _printf.c
@@ -1,15 +1,5 @@
 #include "_printf.h"
 
-void ft_put_str(char *str, int *len) {
-	if (!str) str = "(null)";
-	while (*str)
-		*len += write(1, str++, 1);
-}
-
-void ft_printchar(char c, int *len) {
-	*len += write(1, &c, 1);
-}
-
 int _printf(const char *format, ...)
 {
 	int len;
